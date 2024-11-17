94d28e3caa5306913f84905c063ba252f743b820
Hamzasspace <hamzahrizispace@gmail.com>
2024-11-18 00:21:58 +0100

    Seaborn KDE PLot

--- kdf.py
+++ kdf.py
@@ -1,128 +1,20 @@
-#  This code demonstrates Kernel Density Estimation (KDE) by manually calculating and plotting the KDE for a given dataset using different bandwidth
-
-# dataset
-x = [1.33, 0.3, 0.97, 1.1, 0.1, 1.4, 0.4]
-# brandwidth
-h = 0.3
-
+import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
 
-# the Kernel function
-def K(x):
-    return np.exp(-x**2/2)/np.sqrt(2*np.pi) # this function represents the contribution of a single data point to the density estimation
-
+# dataset
+x = [1.33, 0.3, 0.97, 1.1, 0.1, 1.4, 0.4]
 
 # dummy dataset
 dataset = np.array([1.33, 0.3, 0.97, 1.1, 0.1, 1.4, 0.4])
 
-# x-value range for plotting KDEs
-x_range = np.linspace(dataset.min()-0.3, dataset.max()+0.3, num=600) #for better visualization
-
-
-# bandwith values for experimentation
-H = [0.3, 0.1, 0.03]
-n_samples = dataset.size
-
-# line properties for different bandwith values
-color_list = ['goldenrod', 'black', 'maroon']
-alpha_list = [0.8, 1, 0.8] #transparency
-width_list = [1.7,2.5,1.7]
-
-# Creating the KDE Plot
-plt.figure(figsize=(10,4))
-# iterate over bandwith values
-for h, color, alpha, width in zip(H, color_list, alpha_list, width_list):
-    total_sum = 0
-    # iterate over datapoints
-    for i, xi in enumerate(dataset):
-        total_sum += K((x_range - xi) / h)
-        plt.annotate(r'$x_{}$'.format(i+1),
-                     xy=[xi, 0.13],
-                     horizontalalignment='center',
-                     fontsize=18,
-                    )
-    # Density Estimation
-    y_range = total_sum/(h*n_samples)
-    plt.plot(x_range, y_range, 
-             color=color, alpha=alpha, linewidth=width, 
-             label=f'{h}')
-    # Marking Data Points
-    plt.plot(dataset, np.zeros_like(dataset) , 's', 
-             markersize=8, color='black')
-# Final Plot Customizations
-plt.xlabel('$x$', fontsize=22)
-plt.ylabel('$f(x)$', fontsize=22, rotation='horizontal', labelpad=20)
-plt.legend(fontsize=14, shadow=True, title='$h$', title_fontsize=16)
-
-import seaborn as sns
-sns.set()
-
-fig, ax = plt.subplots(figsize=(10,4))
-
-sns.kdeplot(ax=ax, data=dataset, 
+# Seaborn KDE PLot
+sns.kdeplot(data=dataset, 
             bw_adjust=0.3,
             linewidth=2.5, fill=True)
+plt.title("Seaborn KDE PLot", fontsize=16)
+plt.xlabel("Values", fontsize=12)
+plt.ylabel("Density", fontsize=12)
 
-# plot datapoints
-ax.plot(dataset, np.zeros_like(dataset) + 0.05, 's', 
-        markersize=8, color='black')
-for i, xi in enumerate(dataset):
-    plt.annotate(r'$x_{}$'.format(i+1),
-                 xy=[xi, 0.1],
-                 horizontalalignment='center',
-                 fontsize=18,
-                )
-
-from sklearn.neighbors import KernelDensity
-
-dataset = np.array([1.33, 0.3, 0.97, 1.1, 0.1, 1.4, 0.4])
-
-# KernelDensity requires 2D array
-dataset = dataset[:, np.newaxis]
-
-# fit KDE to the dataset
-kde = KernelDensity(kernel='gaussian', bandwidth=0.1).fit(dataset)
-
-# x-value range for plotting KDE
-x_range = np.linspace(dataset.min()-0.3, dataset.max()+0.3, num=600)
-
-# compute the log-likelihood of each sample
-log_density = kde.score_samples(x_range[:, np.newaxis])
-
-plt.figure(figsize=(10,4))
-# put labels over datapoints
-for i, xi in enumerate(dataset):
-    plt.annotate(r'$x_{}$'.format(i+1),
-                 xy=[xi, 0.07],
-                 horizontalalignment='center',
-                 fontsize=18)
-
-# draw KDE curve
-plt.plot(x_range, np.exp(log_density), 
-         color='gray', linewidth=2.5)
-
-# draw boxes representing datapoints
-plt.plot(dataset, np.zeros_like(dataset) , 's', 
-         markersize=8, color='black')
-    
-plt.xlabel('$x$', fontsize=22)
-plt.ylabel('$f(x)$', fontsize=22, rotation='horizontal', labelpad=24)
-
-# Generate random samples from the model
-synthetic_data = kde.sample(100)
-
-plt.figure(figsize=(10,4))
-
-# draw KDE curve
-plt.plot(x_range, np.exp(log_density), 
-         color='gray', linewidth=2.5)
-
-# draw boxes representing datapoints
-plt.plot(synthetic_data, np.zeros_like(synthetic_data) , 's', 
-         markersize=6, color='black', alpha=0.5)
-    
-plt.xlabel('$x$', fontsize=22)
-plt.ylabel('$f(x)$', fontsize=22, rotation='horizontal', labelpad=24)
 
 plt.show()
